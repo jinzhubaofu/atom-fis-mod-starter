ca92fa30770dcf40c05e20ab82f5ea7adfc54694
lupengyu <lupengyu@baidu.com>
2017-07-03 10:27:16 +0800

    fix spell mistoke

--- scripts/webserver/index.php
+++ scripts/webserver/index.php
@@ -8,7 +8,7 @@
 date_default_timezone_set("UTC");
 
 require_once(__DIR__.'/atom.php');
-require_once(__DIR__.'/Resource.class.php');
+require_once(__DIR__.'/Resource.class.php');
 
 $root = getcwd();
 
